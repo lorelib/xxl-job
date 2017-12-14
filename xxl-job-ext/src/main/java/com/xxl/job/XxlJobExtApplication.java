b42f24e531b34120d09fde2c5c9141a17cbb6318
listening <893979653@qq.com>
2017-12-14 15:25:06 +0800

    将xxl-job-admin合并到xxl-job-ext中执行，无需再启动xxl-job-admin；并且配置打成war包放到tomcat执行的方式

--- xxl-job-ext/src/main/java/com/xxl/job/XxlJobExtApplication.java
+++ xxl-job-ext/src/main/java/com/xxl/job/XxlJobExtApplication.java
@@ -2,6 +2,8 @@ package com.xxl.job;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
 
 /**
@@ -11,11 +13,17 @@ import org.springframework.context.annotation.ImportResource;
  * @since 1.0
  */
 @SpringBootApplication
-@ImportResource({
+@ImportResource("classpath:spring/*.xml")
+/*@ImportResource({
     "classpath:spring/applicationcontext-base.xml",
-    "classpath:spring/applicationcontext-xxl-job-admin.xml"})
-public class XxlJobExtApplication {
+    "classpath:spring/applicationcontext-xxl-job-admin.xml"})*/
+public class XxlJobExtApplication extends SpringBootServletInitializer {
   public static void main(String[] args) {
     SpringApplication.run(XxlJobExtApplication.class, args);
+  }
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(XxlJobExtApplication.class);
   }
 }
